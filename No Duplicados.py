4bb3259665f687d547434b004c9ab8214bd83bec
dianalindo <josefinalindofigueroa@gmail.com>
2023-05-02 20:28:02 -0500

    Este lo acabo de hacer Pablo

--- No Duplicados.py
+++ No Duplicados.py
@@ -1,7 +1,20 @@
-def pongestNoReaptString_v1(s):
-    start = 0
-    max = 0
-    charIndex = {}
+def longitud (total: str) -> str:
+    letras = len(total)
+    cofre = set()
+    inicio1 = inicio2 = 0
+    resultado = ""
 
-    for windowEnd in range(len(s)):
-        euf
+    while inicio2 < letras:
+        if total[inicio2] not in cofre:
+            cofre.add(total[inicio2])
+            inicio2 += 1
+            if len(cofre) > len(resultado):
+                resultado = total[inicio1:inicio2]
+        else:
+            cofre.remove(total[inicio1])
+            inicio1 += 1
+
+    return resultado
+cadena = "aaaaaaaaaaaaaaaafaaafaaafaaafaaafaaafaaafaafaafafaaaaaaaaaaaaaazqlñaaaaaaaaaaaaaaaaaa"
+resultado = longitud(cadena)
+print(resultado)
